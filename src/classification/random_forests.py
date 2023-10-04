7263719fe64e37617e7e734a2fcf02987a0ce3fa
Bharat Soni <bharatsoni@Bharats-MacBook-Air.local>
2023-10-04 10:42:18 +1000

    ml1_final

--- src/classification/random_forests.py
+++ src/classification/random_forests.py
@@ -4,69 +4,83 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 
-class MachineLearning():
+class RandomFClassifier:
 
-    def __init__(self):
-        
+    def __init__(self, dataset_path, dataset_size=400000):
         print("Loading dataset ...")
-        
-        self.flow_dataset = pd.read_csv('../../data/NF-UNSW-NB15.csv')
-        self.flow_dataset = self.flow_dataset.head(400000)
-
+        self.flow_dataset = pd.read_csv(dataset_path)
         self.flow_dataset.iloc[:, 0] = self.flow_dataset.iloc[:, 0].str.replace('.', '')
         self.flow_dataset.iloc[:, 2] = self.flow_dataset.iloc[:, 2].str.replace('.', '')
+        self.flow_dataset = self.flow_dataset.head(dataset_size)
 
     def flow_training(self, n_estimators=10, criterion='entropy', max_depth=None):
         print("Flow Training with n_estimators={}, criterion={}, max_depth={} ...".format(n_estimators, criterion, max_depth))
-        
-        X_flow = self.flow_dataset.iloc[:, :-1].values
-        X_flow = X_flow.astype('float64')
 
+        X_flow = self.flow_dataset.iloc[:, :-1].values.astype('float64')
         y_flow = self.flow_dataset.iloc[:, -1].values
 
         X_flow_train, X_flow_test, y_flow_train, y_flow_test = train_test_split(X_flow, y_flow, test_size=0.3, random_state=0)
 
         classifier = RandomForestClassifier(n_estimators=n_estimators, criterion=criterion, max_depth=max_depth, random_state=0)
         flow_model = classifier.fit(X_flow_train, y_flow_train)
+        return self.evaluate_model(X_flow_test, y_flow_test, flow_model)  # Return the F1 score
 
-        y_flow_pred = flow_model.predict(X_flow_test)
-
+    def train_classifier(self, classifier):
         print("------------------------------------------------------------------------------")
+        print(f"{classifier.__class__.__name__.upper()} ...")
+        self.classifier = classifier
+        self.flow_model = self.classifier.fit(self.X_flow_train, self.y_flow_train)
+        self.evaluate_model()
 
-        print("confusion matrix")
-        cm = confusion_matrix(y_flow_test, y_flow_pred)
+    def evaluate_model(self, X_test, y_test, model):
+        print("------------------------------------------------------------------------------")
+        y_flow_pred = model.predict(X_test)
+
+        print("Confusion matrix")
+        cm = confusion_matrix(y_test, y_flow_pred)
         print(cm)
 
-        acc = accuracy_score(y_flow_test, y_flow_pred)
-        print("Accuracy = {0:.2f} %".format(acc*100))
+        TP = cm[1, 1]  
+        TN = cm[0, 0]  
+        FP = cm[0, 1]  
+        FN = cm[1, 0]  
 
-        precision = precision_score(y_flow_test, y_flow_pred)
-        recall = recall_score(y_flow_test, y_flow_pred)
-        f1 = f1_score(y_flow_test, y_flow_pred)
+        print("True Positives = {}".format(TP))
+        print("True Negatives = {}".format(TN))
+        print("False Positives = {}".format(FP))
+        print("False Negatives = {}".format(FN))
 
-        print("Precision = {0:.2f}".format(precision))
-        print("Recall = {0:.2f}".format(recall))
-        print("F1 Score = {0:.2f}".format(f1))
+        acc = accuracy_score(y_test, y_flow_pred)
+        print("Success Accuracy = {:.2f} %".format(acc * 100))
 
-        return f1  
+        precision = precision_score(y_test, y_flow_pred)
+        recall = recall_score(y_test, y_flow_pred)
+        f1 = f1_score(y_test, y_flow_pred)
+
+        print("Precision = {:.2f}".format(precision))
+        print("Recall = {:.2f}".format(recall))
+        print("F1-score = {:.2f}".format(f1))
+        print("------------------------------------------------------------------------------")
+        return f1 
+
 
 def main():
+    dataset_path = '../../data/NF-UNSW-NB15.csv'
     start = datetime.now()
-    
-    ml = MachineLearning()
-    
+    rf_classifier = RandomFClassifier(dataset_path)
+    rf_classifier.flow_training()
     n_estimators_values = [10, 50, 100]
     criterion_values = ['gini', 'entropy']
     max_depth_values = [None, 10, 20]
 
-    best_f1 = -1  
+    best_f1 = 0  
     best_params = None
     
     for n_estimators in n_estimators_values:
         for criterion in criterion_values:
             for max_depth in max_depth_values:
-                f1 = ml.flow_training(n_estimators, criterion, max_depth)
-                if f1 > best_f1:
+                f1 = rf_classifier.flow_training(n_estimators, criterion, max_depth)
+                if f1 is not None and f1 > best_f1:
                     best_f1 = f1
                     best_params = (n_estimators, criterion, max_depth)
     
@@ -74,6 +88,8 @@ def main():
     print("Training time: ", (end-start)) 
     print("Best Parameter Combination (n_estimators, criterion, max_depth):", best_params)
     print("Best F1 Score:", best_f1)
+    end = datetime.now()
+    print("Training time: ", (end - start))
 
 if __name__ == "__main__":
     main()
--- src/classification/svm.py
+++ src/classification/svm.py
@@ -1,64 +1,75 @@
-from datetime import datetime
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+from datetime import datetime
 
-class MachineLearning():
+class SVMClassifier:
 
-    def __init__(self):
-
+    def __init__(self, dataset_path, dataset_size=400000):
         print("Loading dataset ...")
-
-        self.flow_dataset = pd.read_csv('../../data/NF-UNSW-NB15.csv')
-
+        self.flow_dataset = pd.read_csv(dataset_path)
         self.flow_dataset.iloc[:, 0] = self.flow_dataset.iloc[:, 0].str.replace('.', '')
         self.flow_dataset.iloc[:, 2] = self.flow_dataset.iloc[:, 2].str.replace('.', '')
+        self.flow_dataset = self.flow_dataset.head(dataset_size)
 
     def flow_training(self):
-
         print("Flow Training ...")
 
-        self.flow_dataset = self.flow_dataset.head(400000)
-
-        X_flow = self.flow_dataset.iloc[:, :-1].values
-        X_flow = X_flow.astype('float64')
-
+        X_flow = self.flow_dataset.iloc[:, :-1].values.astype('float64')
         y_flow = self.flow_dataset.iloc[:, -1].values
 
         X_flow_train, X_flow_test, y_flow_train, y_flow_test = train_test_split(X_flow, y_flow, test_size=0.3, random_state=0)
 
         classifier = SVC(kernel='rbf', random_state=0)
         flow_model = classifier.fit(X_flow_train, y_flow_train)
+        self.evaluate_model(X_flow_test, y_flow_test, flow_model)
 
-        y_flow_pred = flow_model.predict(X_flow_test)
-
+    def train_classifier(self, classifier):
         print("------------------------------------------------------------------------------")
+        print(f"{classifier.__class__.__name__.upper()} ...")
+        self.classifier = classifier
+        self.flow_model = self.classifier.fit(self.X_flow_train, self.y_flow_train)
+        self.evaluate_model()
+
+    def evaluate_model(self, X_test, y_test, model):
+        print("------------------------------------------------------------------------------")
+        y_flow_pred = model.predict(X_test)
 
         print("Confusion matrix")
-        cm = confusion_matrix(y_flow_test, y_flow_pred)
+        cm = confusion_matrix(y_test, y_flow_pred)
         print(cm)
 
-        acc = accuracy_score(y_flow_test, y_flow_pred)
-        print("Success Accuracy = {0:.2f} %".format(acc*100))
+        TP = cm[1, 1]  
+        TN = cm[0, 0]  
+        FP = cm[0, 1]  
+        FN = cm[1, 0]  
 
-        precision = precision_score(y_flow_test, y_flow_pred)
-        recall = recall_score(y_flow_test, y_flow_pred)
-        f1 = f1_score(y_flow_test, y_flow_pred)
+        print("True Positives = {}".format(TP))
+        print("True Negatives = {}".format(TN))
+        print("False Positives = {}".format(FP))
+        print("False Negatives = {}".format(FN))
 
-        print("Precision = {0:.2f}".format(precision))
-        print("Recall = {0:.2f}".format(recall))
-        print("F1-score = {0:.2f}".format(f1))
+        acc = accuracy_score(y_test, y_flow_pred)
+        print("Success Accuracy = {:.2f} %".format(acc * 100))
+
+        precision = precision_score(y_test, y_flow_pred)
+        recall = recall_score(y_test, y_flow_pred)
+        f1 = f1_score(y_test, y_flow_pred)
+
+        print("Precision = {:.2f}".format(precision))
+        print("Recall = {:.2f}".format(recall))
+        print("F1-score = {:.2f}".format(f1))
         print("------------------------------------------------------------------------------")
 
+
 def main():
+    dataset_path = '../../data/NF-UNSW-NB15.csv'
     start = datetime.now()
-
-    ml = MachineLearning()
-    ml.flow_training()
-
+    svm_classifier = SVMClassifier(dataset_path)
+    svm_classifier.flow_training()
     end = datetime.now()
-    print("Training time: ", (end-start))
+    print("Training time: ", (end - start))
 
 if __name__ == "__main__":
     main()
